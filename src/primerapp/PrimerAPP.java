66d48b98c92f21cbc596011a2447067b6467ea9a
josefigueroa17 <79346092+josefigueroa17@users.noreply.github.com>
2023-04-18 18:39:47 -0600

    Primer Commit

---
+++ src/primerapp/PrimerAPP.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primerapp;
+import javax.swing.JOptionPane;
+
+public class PrimerAPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        JOptionPane.showMessageDialog(null, "Hola perro");
+    }
+    
+}
